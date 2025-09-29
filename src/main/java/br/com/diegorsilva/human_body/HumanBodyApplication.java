667980f7ace91bc6a833a794c7f2997d3d8009eb
Diego Rodrigues da Silva <diegorodrigues97@yahoo.com.br>
2025-09-28 23:34:50 -0300

    Initial commit

---
+++ src/main/java/br/com/diegorsilva/human_body/HumanBodyApplication.java
@@ -0,0 +1,13 @@
+package br.com.diegorsilva.human_body;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanBodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diegorsilva/human_body/HumanBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegorsilva.human_body;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanBodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
